87f9f6da34ed32fa3b498aad142c12b24e56d44b
yohannes.codes <yohannes.codes@gmail.com>
2025-11-08 03:06:37 -0500

    refactor(BooleanValidator): streamline _parse method for improved readability and logic

--- src/schemas/boolean.ts
+++ src/schemas/boolean.ts
@@ -15,68 +15,37 @@ export class BooleanValidator extends BaseValidator<boolean> {
   }
 
   protected _parse(input: any, path: string = ""): SurelyResult<boolean> {
-    let output = input;
+    let output: boolean | undefined;
 
-    if (typeof output !== "boolean") {
-      if (this._strict) {
-        return respond.error.type("boolean", input, path);
-      } else {
-        if (
-          output === "true" ||
-          output === "TRUE" ||
-          output === "True" ||
-          output === 1 ||
-          output === "1" ||
-          output === "yes" ||
-          output === "YES" ||
-          output === "Yes" ||
-          output === "on" ||
-          output === "ON" ||
-          output === "On" ||
-          output === "Y" ||
-          output === "y" ||
-          output === "T" ||
-          output === "t"
-        ) {
-          output = true;
-        } else if (
-          output === "false" ||
-          output === "FALSE" ||
-          output === "False" ||
-          output === 0 ||
-          output === "0" ||
-          output === "no" ||
-          output === "NO" ||
-          output === "No" ||
-          output === "off" ||
-          output === "OFF" ||
-          output === "Off" ||
-          output === "N" ||
-          output === "n" ||
-          output === "F" ||
-          output === "f"
-        ) {
-          output = false;
-        } else return respond.error.type("boolean", input, path);
-      }
-    }
+    if (typeof input !== "boolean")
+      if (this._strict) return respond.error.type("boolean", input, path);
+      else if (typeof input === "number")
+        if (input === 1) output = true;
+        else if (input === 0) output = false;
+        else output = undefined;
+      else if (typeof input === "string")
+        if (input === "true") output = true;
+        else if (input === "false") output = false;
+        else output = undefined;
+      else output = undefined;
+    else output = input;
+
+    if (output === undefined) return respond.error.type("boolean", input, path);
 
     if (this._truthy !== undefined) {
-      if (this._truthy === true && !output) {
+      if (this._truthy === true && !output)
         return respond.error.generic(
           `[truthy] Expected truthy value, but received falsy value`,
           path,
           input
         );
-      }
 
-      if (this._truthy === false && output) {
+      if (this._truthy === false && output)
         return respond.error.generic(
           `[falsy] Expected falsy value, but received truthy value`,
           path,
           input
         );
-      }
     }
 
     return respond.success(output);
